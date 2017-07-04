3562760711d01f4d59b3f61268f365b843ae4fc0
jonathanandrewsuk <jonathanandrewsuk@gmail.com>
2017-07-04 11:43:21 -0400

    spicy text transparency fix

--- src/components/show.react.js
+++ src/components/show.react.js
@@ -18,25 +18,29 @@ export default class ShowPage extends React.Component {
     return (
 
   <container id="container">
+
     <div id="content">
-    <h1 className="animated intensifies" id={this.props.words[0].text_theme}> {this.props.term_1} </h1>
+
       <div className= "horizontal div3" id={this.props.words[0].gif_theme}>
         <img className="background-image" src={"https://media.giphy.com/media/" + this.props.images.images_1[`${this.props.shuffle_1}`] + "/giphy.gif"} alt="Image 1"/>
       </div>
+      <h1 className="animated intensifies" id={this.props.words[0].text_theme}> {this.props.term_1} </h1>
     </div>
 
     <div id="primary">
-      <h1 className="animated intensifies" id={this.props.words[1].text_theme}> {this.props.term_2} </h1>
+
     <div className="horizontal div3" id={this.props.words[1].gif_theme}>
           <img className="background-image" src={"https://media.giphy.com/media/" + this.props.images.images_2[`${this.props.shuffle_2}`] + "/giphy.gif"} alt="Image 2"/>
         </div>
+        <h1 className="animated intensifies" id={this.props.words[1].text_theme}> {this.props.term_2} </h1>
     </div>
 
     <div id="secondary">
-    <h1 className="animated intensifies" id={this.props.words[2].text_theme}> {this.props.term_3} </h1>
+
     <div className="horizontal div3" id={this.props.words[2].gif_theme}>
         <img className="background-image" src={"https://media.giphy.com/media/" + this.props.images.images_3[`${this.props.shuffle_3}`] + "/giphy.gif"} alt="Image 3"/>
       </div>
+      <h1 className="animated intensifies" id={this.props.words[2].text_theme}> {this.props.term_3} </h1>
     </div>
   </container>
     );
--- src/components/show_saved.react.js
+++ src/components/show_saved.react.js
@@ -30,24 +30,24 @@ export default class ShowPageSaved extends React.Component {
 
     <container id="container">
       <div id="content">
-      <h1 className="animated intensifies" id={this.props.words[0].text_theme}>  {this.props.words[0].text} </h1>
         <div className= "horizontal div3" id={this.props.words[0].gif_theme}>
           <img className="background-image" src={"https://media.giphy.com/media/" + this.props.words[0].gif_id + "/giphy.gif"} alt="Image 1"/>
         </div>
+        <h1 className="animated intensifies" id={this.props.words[0].text_theme}>  {this.props.words[0].text} </h1>
       </div>
 
       <div id="primary">
-      <h1 className="animated intensifies" id={this.props.words[1].text_theme}>{this.props.words[1].text} </h1>
       <div className="horizontal div3" id={this.props.words[1].gif_theme}>
             <img className="background-image" src={"https://media.giphy.com/media/" + this.props.words[1].gif_id + "/giphy.gif"} alt="Image 2"/>
           </div>
+          <h1 className="animated intensifies" id={this.props.words[1].text_theme}>{this.props.words[1].text} </h1>
       </div>
 
       <div id="secondary">
-      <h1 className="animated intensifies" id={this.props.words[2].text_theme}> {this.props.words[2].text} </h1>
       <div className="horizontal div3" id={this.props.words[2].gif_theme}>
           <img className="background-image" src={"https://media.giphy.com/media/" + this.props.words[2].gif_id + "/giphy.gif"} alt="Image 3"/>
         </div>
+        <h1 className="animated intensifies" id={this.props.words[2].text_theme}> {this.props.words[2].text} </h1>
       </div>
     </container>
       );
